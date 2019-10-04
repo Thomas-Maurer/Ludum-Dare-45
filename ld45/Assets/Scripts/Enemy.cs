d0f2c90af706aca3529412aa154d367d09baed81
Thomas <thomas.wellan@gmail.com>
2019-10-05 01:50:59 +0200

    Add enemy

---
+++ ld45/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.level = this.generateLevel();
+        Debug.Log("Hello: " + this.level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // Generate the level of the Enemy
+    int generateLevel()
+    {
+        return NumbersUtils.RandomNumber(1, 69);
+    }
+}
---
+++ ld45/Assets/Scripts/Utils/NumbersUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumbersUtils : MonoBehaviour
+{
+    // Generate a random number between two numbers
+public static int RandomNumber(int min, int max)
+    {
+        System.Random random = new System.Random();
+        return random.Next(min, max);
+    }
+}
