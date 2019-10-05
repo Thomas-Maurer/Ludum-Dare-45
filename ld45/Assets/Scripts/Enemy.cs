93a22fef16d488e4a48762e86569d657897c6c42
Thomas <thomas.wellan@gmail.com>
2019-10-05 09:33:47 +0200

    Moving working

--- ld45/Assets/Scripts/Enemy.cs
+++ ld45/Assets/Scripts/Enemy.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private int level;
+    private int hp;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private Transform target;
+    private double speed;
+    public Transform Target { get => target; set => target = value; }
+    public double Speed { get => speed; set => speed = value; }
+
     // Start is called before the first frame update
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        
         this.level = this.generateLevel();
         Debug.Log("Hello: " + this.level);
+        this.hp = this.generateHp();
+        this.Speed = this.generateSpeed();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     // Generate the level of the Enemy
     int generateLevel()
     {
         return NumbersUtils.RandomNumber(1, 69);
     }
+    //Generate HP by level.
+    //Maths formula = level * 3
+    int generateHp()
+    {
+        return this.level * 3;
+    }
+
+    //Generate Speed by level.
+    //Maths formula = level * 0.1
+    double generateSpeed()
+    {
+        return this.level * 0.1;
+    }
+
 }
